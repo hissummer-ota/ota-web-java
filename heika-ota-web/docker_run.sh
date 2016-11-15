301953049d09d95a7ec0b8cb40f9aca33132b63b
杨剑 <hn.wendy@qq.com>
2016-11-15 11:26:37 +0800

    support docker

--- heika-ota-web/docker_run.sh
+++ heika-ota-web/docker_run.sh
@@ -13,4 +13,4 @@ if [ $COUNT = 1 ]; then
     docker rm $INSTANCENAME
 fi
 
-docker run -it -d --name $INSTANCENAME -p 9008:8080 -p 9009:8443 -v /home/test/qa-app-tomcats/heika-ota-9001-9003/webapps/otadata:/usr/local/tomcat/webapps/otadata $IMAGENAME
+docker run -it -d --name $INSTANCENAME -p 9100:8080 -p 9101:8443 -v /home/test/qa-app-tomcats/heika-ota-9001-9003/webapps/otadata:/usr/local/tomcat/webapps/otadata $IMAGENAME
